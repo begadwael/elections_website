4e5b43581b505ffdddbf00452c53e8456a961f8c
Begadmoussa <56642036+begadwael@users.noreply.github.com>
2025-03-13 00:46:28 +0400

    build fix

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -200,7 +200,7 @@ export default function HeroSection() {
           variants={fadeInUp}
           className="text-4xl italic text-white/80 mb-8"
         >
-          "For Students, By Students"
+          &quotFor Students, By Students&quot
         </motion.p>
 
         {/* Call to Action */}
